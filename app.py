e2aac492c22200217c7d2833450540b1d211781e
Frank Kotsianas <frankkotsianas@gmail.com>
2020-03-24 21:15:12 -0400

    AVS: apply semantic-ui to calculator, require fields, and submit successfully

--- app.py
+++ app.py
@@ -14,12 +14,19 @@ def result():
       result = request.form
       keys = ['tFlow','pressure','area','yes_no3','yes_no4','yes_no5','yes_no6','yes_no7','yes_no8','thickness','diameter']
       arr = []
+      print("YES3")
+      from pprint import pprint
+      pprint(request.form)
       for k in keys:
-          val = request.form[k]
-          if val == 'on':
+          val = request.form.get(k, None)
+
+          # translate frontend radio button "value"s into 0/1
+          if val == 'true':
               val = 1
-          elif val == 'off':
+          elif val == 'false':
               val = 0
+
+          print(k, request.form.get(k, None), float(val))
           arr.append(float(val))
       arr = np.array([arr])
       yh_syn = get_model_output(arr)
